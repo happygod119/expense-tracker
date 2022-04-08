f5f1c378a288ca7344c23971a2d9dc9793d28aad
happygod119 <happ0983493655@gmail.com>
2022-04-08 19:24:53 +0800

    feat:add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express"); // 載入Express
+const exphbs = require("express-handlebars"); // 載入handlebars
 
+const routes = require("./routes"); // 引用路由器
 require("./config/mongoose"); // 引用mongoose;
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+//設定使用handlebars
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(routes); // 將 request 導入路由器
 
 // 設定port
 app.listen(PORT, () => {
