3b15b547b86e5ba4c3b1367915b69ae9deb13785
happygod119 <happ0983493655@gmail.com>
2022-04-08 20:42:44 +0800

    feat:add update&delete  function

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express"); // 載入Express
+const session = require("express-session"); // 載入express-session
 const exphbs = require("express-handlebars"); // 載入handlebars
-const bodyParser = require("body-parser");// 載入body-parser
+const bodyParser = require("body-parser"); // 載入body-parser
 const methodOverride = require("method-override"); //載入method-override
+const flash = require("connect-flash"); // 引用 connect-flash
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const routes = require("./routes"); // 引用路由器
 require("./config/mongoose"); // 引用mongoose;
@@ -13,9 +18,8 @@ const PORT = 3000;
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
-
 app.use(bodyParser.urlencoded({ extended: true })); // 使用body-parser
-app.use(methodOverride("_method")); // 使用methodOverride 
+app.use(methodOverride("_method")); // 使用methodOverride
 
 app.use(routes); // 將 request 導入路由器
 
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -15,8 +15,48 @@ router.get("/new", (req, res) => {
 });
 router.post("/", (req, res) => {
   const record = req.body;
-  record.userId = req.user._id;
+  // record.userId = req.user._id;
   return Record.create(record)
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+//- 修改資料
+router.get("/:id/edit", (req, res) => {
+  const _id = req.params.id;
+  const userId = req.user._id;
+  Record.findOne({ _id, userId })
+    .populate("categoryId")
+    .lean()
+    .then((record) => {
+      const selected = record.categoryId;
+      Category.find({ _id: { $ne: record.categoryId } })
+        .lean()
+        .then((category) => {
+          res.render("edit", { record, selected, category });
+        });
+    })
+    .catch((err) => console.log(err));
+});
+router.put("/:id", (req, res) => {
+  const editedRecord = req.body;
+  const _id = req.params.id;
+  const userId = req.user._id;
+  return Record.findOne({ _id, userId })
+    .then((record) => {
+      record = Object.assign(record, editedRecord);
+      return record.save();
+    })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+//- 刪除資料
+router.delete("/:id", (req, res) => {
+  const userId = req.user._id;
+  const _id = req.params.id;
+  return Record.findOne({ _id, userId })
+    .then((record) => record.remove())
     .then(() => res.redirect("/"))
     .catch((err) => console.log(err));
 });
