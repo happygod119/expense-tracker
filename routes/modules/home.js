f0d5584bd0848bbe607de4a31e360d5e22e39808
happygod119 <happ0983493655@gmail.com>
2022-04-09 10:45:42 +0800

    feat: 修改home頁

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+const Category = require("../../models/category");
+const Record = require("../../models/record");
+
 router.get("/", (req, res) => {
-  res.render("index");
+  const userId = req.user._id;
+  Category.find()
+    .lean()
+    .then((category) => {
+      return Record.find({ userId })
+        .populate("categoryId")
+        .lean()
+        .then((record) => {
+          let totalAmount = 0;
+          record.forEach((data) => (totalAmount += data.amount));
+          res.render("index", { record, category, totalAmount });
+        })
+        .catch((err) => console.log(err));
+    });
 });
 
 module.exports = router;
